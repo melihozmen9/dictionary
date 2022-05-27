d207baf48d5b2ababba1a0966d8bf2b629bb7540
melihozmen9 <105754659+melihozmen9@users.noreply.github.com>
2022-05-27 23:38:05 +0300

    Initial Commit

---
+++ dictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dictionary
+//
+//  Created by Kullanici on 27.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
